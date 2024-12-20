4ad323679984407a63facfc8a496842b2f15efa4
RodolfoHRE <rodolfohre@gmail.com>
2024-12-20 15:53:08 -0300

    edit e delete do comment feitos e testados no back
    rever alguns detalhes como a imagem que ficou comentado no model e no serializer, além da data de update que até o momento não tem

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -45,3 +45,54 @@ class CommentRegisterView(APIView):
             return Response({'detail': f'An unexpected error occurred. {e}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response({'detail': 'Comment created successfully.', 'id': comment.id}, status=status.HTTP_201_CREATED)
+
+
+class CommentEditView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, comment_id):
+        # Obtém o comentário pelo ID
+        comment = get_object_or_404(Comment, id=comment_id)
+
+        account = request.user.account
+        try:
+            user_profile = UserProfile.objects.get(account=account, active=True)
+        except UserProfile.DoesNotExist:
+            return Response({"detail": "Invalid or inactive user."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Verifica se o usuário logado é o dono do comentário
+        if comment.user_profile != user_profile:
+            return Response({"detail": "You do not have permission to edit this comment."}, status=status.HTTP_403_FORBIDDEN)
+
+        # Atualiza os dados do comentário
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"success": "Comment updated successfully."}, status=status.HTTP_200_OK)
+        else:
+            return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDeleteView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, comment_id):
+        # Obtém o comentário pelo ID
+        comment = get_object_or_404(Comment, id=comment_id)
+
+        account = request.user.account
+        try:
+            user_profile = UserProfile.objects.get(account=account, active=True)
+        except UserProfile.DoesNotExist:
+            return Response({"detail": "Active user profile not found."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Verifica se o usuário logado é o dono do comentário
+        if comment.user_profile != user_profile:
+            return Response({"detail": "You do not have permission to delete this comment."}, status=status.HTTP_403_FORBIDDEN)
+
+        # Deleta o comentário
+        comment.delete()
+
+        return Response({"detail": "Comment deleted successfully."}, status=status.HTTP_200_OK)
