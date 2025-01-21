5a60d9a73d4c7bc905be48d8c6d7b9b7adf202f4
Marcos <mhmcamp@gmail.com>
2025-01-21 01:01:04 -0300

    denounce

--- frontend/src/views/EventDetailPage.vue
+++ frontend/src/views/EventDetailPage.vue
@@ -92,6 +92,7 @@
               </button>
 
               <button type="button"
+                @click="denounce"
                 class="px-6 py-3 rounded-lg hover:bg-gray-100 text-white transition-colors duration-200 mt-4" 
                 style="background-color: rgb(252, 3, 94);">
                 Denunciar
@@ -247,7 +248,7 @@
                             class="w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100">
                             Deletar
                           </button>
-                          <button @click="menuStates[comment.id] = false"
+                          <button @click="() => {menuStates[comment.id] = false; denounce()}"
                             class="w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100">
                             Reportar
                           </button>
@@ -705,7 +706,9 @@ const createPoll = () => {
   showPostButton.value = true
   cancelPoll()
 }
-
+const denounce = ()=>{
+  toast.success("Denúncia realizada, nossos moderadores estarão de olho 👁️‍🗨️")
+}
 const imagePreview = ref(null);
 const selectedImage = ref(null);
 const imageInput = ref(null);
